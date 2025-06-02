673c0cf10efb0b84ec24faa5f53804e38c8bf2f5
hoang4325 <hoangheir@gmail.com>
2025-06-02 20:17:57 +0700

    main

---
+++ src/com/view/component/PanelLoginAndRegister.java
@@ -0,0 +1,127 @@
+
+package com.view.component;
+
+import com.view.swing.Button;
+import com.view.swing.MyPasswordField;
+import com.view.swing.MyTextField;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import net.miginfocom.swing.MigLayout;
+
+public class PanelLoginAndRegister extends javax.swing.JLayeredPane {
+
+    
+    public PanelLoginAndRegister() {
+        initComponents();
+        initRegister();
+        initLogin();
+        login.setVisible(false);
+        register.setVisible(true);
+    }
+    private void initRegister(){
+        register.setLayout(new MigLayout("wrap", "push[center]push", "push[]25[]10[]10[]25[]push"));
+        JLabel label = new JLabel("Create Account");
+        label.setFont(new Font("sansserif", 1, 30));
+        label.setForeground(new Color(7, 164, 121));
+        register.add(label);
+        MyTextField txtUser = new MyTextField();
+        txtUser.setPrefixIcon(new ImageIcon(getClass().getResource("/com/icon/user.png")));
+        txtUser.setHint("Name");
+        register.add(txtUser, "w 60%");
+        MyTextField txtEmail = new MyTextField();
+        txtEmail.setPrefixIcon(new ImageIcon(getClass().getResource("/com/icon/mail.png")));
+        txtEmail.setHint("Email");
+        register.add(txtEmail, "w 60%");
+        MyPasswordField txtPass = new MyPasswordField();
+        txtPass.setPrefixIcon(new ImageIcon(getClass().getResource("/com/icon/pass.png")));
+        txtPass.setHint("Password");
+        register.add(txtPass, "w 60%");
+        Button cmd = new Button();
+        cmd.setBackground(new Color(7, 164, 121));
+        cmd.setForeground(new Color(250, 250, 250));
+        cmd.setText("SIGN UP");
+        register.add(cmd, "w 40%, h 40");
+    }
+    private void initLogin(){
+        login.setLayout(new MigLayout("wrap", "push[center]push", "push[]25[]10[]10[]25[]push"));
+        JLabel label = new JLabel("Sign In");
+        label.setFont(new Font("sansserif", 1, 30));
+        label.setForeground(new Color(7, 164, 121));
+        login.add(label);
+        MyTextField txtEmail = new MyTextField();
+        txtEmail.setPrefixIcon(new ImageIcon(getClass().getResource("/com/icon/mail.png")));
+        txtEmail.setHint("Email");
+        login.add(txtEmail, "w 60%");
+        MyPasswordField txtPass = new MyPasswordField();
+        txtPass.setPrefixIcon(new ImageIcon(getClass().getResource("/com/icon/pass.png")));
+        txtPass.setHint("Password");
+        login.add(txtPass, "w 60%");
+        Button cmd = new Button();
+        cmd.setBackground(new Color(7, 164, 121));
+        cmd.setForeground(new Color(250, 250, 250));
+        cmd.setText("SIGN IN");
+        login.add(cmd, "w 40%, h 40");
+    }
+    
+    public void showRegister(boolean show){
+        if(show){
+            register.setVisible(true);
+            login.setVisible(false);
+        } else {
+            register.setVisible(false);
+            login.setVisible(true);
+        }
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        login = new javax.swing.JPanel();
+        register = new javax.swing.JPanel();
+
+        setLayout(new java.awt.CardLayout());
+
+        login.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout loginLayout = new javax.swing.GroupLayout(login);
+        login.setLayout(loginLayout);
+        loginLayout.setHorizontalGroup(
+            loginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 354, Short.MAX_VALUE)
+        );
+        loginLayout.setVerticalGroup(
+            loginLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        add(login, "card3");
+
+        register.setBackground(new java.awt.Color(255, 255, 255));
+
+        javax.swing.GroupLayout registerLayout = new javax.swing.GroupLayout(register);
+        register.setLayout(registerLayout);
+        registerLayout.setHorizontalGroup(
+            registerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 354, Short.MAX_VALUE)
+        );
+        registerLayout.setVerticalGroup(
+            registerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        add(register, "card2");
+    }// </editor-fold>//GEN-END:initComponents
+
+    
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel login;
+    private javax.swing.JPanel register;
+    // End of variables declaration//GEN-END:variables
+}
