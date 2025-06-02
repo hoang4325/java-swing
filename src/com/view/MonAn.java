0a5306d191dca78820bce06f8644e0f42509f37f
Son <lson5806@gmail.com>
2025-06-02 21:46:22 +0700

    add

---
+++ src/com/view/MonAn.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author homes
+ */
+public class MonAn {
+    private String tenMon;
+    private double gia;
+
+    public MonAn(String tenMon, double gia) {
+        this.tenMon = tenMon;
+        this.gia = gia;
+    }
+
+    public String getTenMon() {
+        return tenMon;
+    }
+
+    public double getGia() {
+        return gia;
+    }
+}
